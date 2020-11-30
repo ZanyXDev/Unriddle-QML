1fb908a1f03ff4328f3e636297241bb388f4a86f
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2020-11-30 14:54:00 +0300

    enable style for app

--- unriddle/src/main.cpp
+++ unriddle/src/main.cpp
@@ -14,6 +14,8 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
+    QQuickStyle::setStyle("Material");
+
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
     if (engine.rootObjects().isEmpty()){
