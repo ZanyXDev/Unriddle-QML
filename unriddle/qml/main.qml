aed620169bda740512f281e5c82b1cd3286fd6f9
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2020-11-30 09:56:42 +0300

    run sumple window

--- unriddle/qml/main.qml
+++ unriddle/qml/main.qml
@@ -1,15 +1,12 @@
 import QtQuick 2.11
 import QtQuick.Controls 2.4
-import QtQuick.Controls.Material 2.4
-import QtQuick.Layouts 1.11
-import QtQuick.Window 2.11
 
-
-ApplicationWindow {
-    id:root
-    width: Screen.width
-    height: Screen.height
+ApplicationWindow
+{
     visible: true
-
-
+    width: 640
+    height: 480
+    title: qsTr("Minimal Qml")
 }
+
+
--- unriddle/src/main.cpp
+++ unriddle/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -9,8 +10,8 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
-    if (engine.rootObjects().isEmpty())
+    if (engine.rootObjects().isEmpty()){
         return -1;
-
+    }
     return app.exec();
 }
